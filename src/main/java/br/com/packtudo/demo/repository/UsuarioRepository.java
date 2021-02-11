d5a10e1ac5d83aba6f84fdf2983ff0adccd39b02
Milson Fabio <packtudo@gmail.com>
2021-02-11 20:20:22 -0300

    Ajuste no pacote de entidades

--- src/main/java/br/com/packtudo/demo/repository/UsuarioRepository.java
+++ src/main/java/br/com/packtudo/demo/repository/UsuarioRepository.java
@@ -3,7 +3,7 @@ package br.com.packtudo.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.packtudo.demo.model.Usuario;
+import br.com.packtudo.demo.model.entity.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
