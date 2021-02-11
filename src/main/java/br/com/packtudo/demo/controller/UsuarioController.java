e4865ff0673ad4f7fff5637c6a8af731c7ec8d07
Milson Fabio <packtudo@gmail.com>
2021-02-11 20:26:38 -0300

    Criação do pacote service

---
+++ src/main/java/br/com/packtudo/demo/controller/UsuarioController.java
@@ -0,0 +1,10 @@
+package br.com.packtudo.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/usuarios")
+public class UsuarioController {
+    
+}
---
+++ src/main/java/br/com/packtudo/demo/service/UsuarioService.java
@@ -0,0 +1,7 @@
+package br.com.packtudo.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+}
